34679e0c4c5c1e016d768c3b229d906c160fda5d
Kevien <18846416619@163.com>
2025-09-04 11:29:52 +0800

    删除data-asset-platform-mvp项目中的多个文件，包括配置文件、页面组件、样式文件和数据文件，以清理项目结构并移除不再使用的代码。

--- data-asset-platform-mvp/src/components/states/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Loading } from '../Loading';
-export { default as EmptyState } from '../EmptyState';
-export { default as ErrorState } from '../ErrorState';
-export { default as SkeletonPlaceholder } from '../SkeletonPlaceholder';
-export { default as StateContainer } from '../StateContainer';
-
-export type { EmptyType } from '../EmptyState';
-export type { ErrorType } from '../ErrorState';
-export type { SkeletonType } from '../SkeletonPlaceholder';
