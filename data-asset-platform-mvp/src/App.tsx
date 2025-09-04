d03f45e418895bd1eed0a21f442340a7d7477971
Kevien <18846416619@163.com>
2025-09-04 10:24:21 +0800

    在App组件中添加资产详情页面的路由配置，增强页面导航功能。

--- data-asset-platform-mvp/src/App.tsx
+++ data-asset-platform-mvp/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import AppLayout from '@/components/Layout'
 import HomePage from '@/pages/Home'
 import DiscoveryPage from '@/pages/Discovery'
+import AssetDetailPage from '@/pages/AssetDetail'
 import './App.css'
 
 
@@ -15,6 +16,7 @@ export default function App() {
       <Route element={<AppLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/discovery" element={<DiscoveryPage />} />
+        <Route path="/assets/:id" element={<AssetDetailPage />} />
         <Route path="/system/users" element={<SystemUsers />} />
       </Route>
     </Routes>
